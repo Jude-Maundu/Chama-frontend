931ee916dacf89447a2afc871de456fba878acd2
Jude-Maundu <judemaundu001@gmail.com>
2026-05-06 21:07:09 +0300

    error handling

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_URL || 'https://chama-backend-laravel-master-dl6jst.free.laravel.cloud/api'
+
 const api = axios.create({
-  baseURL: 'https://chama-backend-laravel-master-dl6jst.free.laravel.cloud/api',
+  baseURL,
   headers: {
     'Content-Type': 'application/json'
   }
@@ -14,5 +16,18 @@ api.interceptors.request.use(config => {
   }
   return config
 })
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    const defaultMessage = 'An error occurred while communicating with the server.'
+    const responseData = error.response?.data
+    const message = responseData?.message || defaultMessage
+
+    // Attach a normalized error message to the thrown error
+    error.userMessage = message
+    return Promise.reject(error)
+  }
+)
 
 export default api
