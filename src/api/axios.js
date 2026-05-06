6f1a6c89ef5e818db53ff6b74065ba001c887570
Jude-Maundu <judemaundu001@gmail.com>
2026-05-06 20:35:38 +0300

    changed the api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://chama-backend-laravel.onrender.com/api',
+  baseURL: 'https://chama-backend-laravel-master-dl6jst.free.laravel.cloud/api',
   headers: {
     'Content-Type': 'application/json'
   }
